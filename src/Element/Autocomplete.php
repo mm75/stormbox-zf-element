7c382929ab2ce435243994b96a9cd30a78e2ac6a
Mario <mario@inovadora.com.br>
2016-06-27 16:16:59 -0300

    Criação da classe Autocomplete.

---
+++ src/Element/Autocomplete.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Element;
+
+/**
+ * Classe responsável por ...
+ *
+ * PHP Version 5.6.0
+ *
+ * @category Autocomplete
+ * @package  Response
+ * @author Jackson Veroneze <jackson@inovadora.com.br>
+ * @author Ladislau Perrony <ladislau.perrony@inovadora.com.br>
+ * @author Mario Mendonça <mario@inovadora.com.br>
+ * @author Mateus Calza <mateus@inovadora.com.br>
+ * @author Patrick Nascimento <patrick@inovadora.com.br>
+ * @license  http://inovadora.com.br/licenca  Inovadora
+ * @link     #
+ * @version 01.00.000
+ */
+class Autocomplete
+{
+    
+}
