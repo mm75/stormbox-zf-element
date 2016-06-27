96898873828b918e9a2f17546202af542ae42516
Mario <mario@inovadora.com.br>
2016-06-27 16:51:58 -0300

    Adicionado referência ZF a classe Element.

--- src/Element/Autocomplete.php
+++ src/Element/Autocomplete.php
@@ -8,7 +8,7 @@ namespace Element;
  * PHP Version 5.6.0
  *
  * @category Autocomplete
- * @package  Response
+ * @package  Element
  * @author Jackson Veroneze <jackson@inovadora.com.br>
  * @author Ladislau Perrony <ladislau.perrony@inovadora.com.br>
  * @author Mario Mendonça <mario@inovadora.com.br>
@@ -18,7 +18,25 @@ namespace Element;
  * @link     #
  * @version 01.00.000
  */
-class Autocomplete
+use Zend_Form_Element_Hidden;
+
+/**
+ * Classe responsável por ...
+ *
+ * PHP Version 5.6.0
+ *
+ * @category Autocomplete
+ * @package  Element
+ * @author Jackson Veroneze <jackson@inovadora.com.br>
+ * @author Ladislau Perrony <ladislau.perrony@inovadora.com.br>
+ * @author Mario Mendonça <mario@inovadora.com.br>
+ * @author Mateus Calza <mateus@inovadora.com.br>
+ * @author Patrick Nascimento <patrick@inovadora.com.br>
+ * @license  http://inovadora.com.br/licenca  Inovadora
+ * @link     #
+ * @version 01.00.000
+ */
+class Autocomplete extends Zend_Form_Element_Hidden
 {
     
 }
