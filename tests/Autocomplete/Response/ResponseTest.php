1dae74ca1e2a5fc0198f6bd77ecf91192028a6e9
Jackson <jackson@jacksonveroneze.com>
2016-06-29 14:53:42 -0300

    Tarefa: refs #11026 Descrição: Ajustar Autocomplete.

---
+++ tests/Autocomplete/Response/ResponseTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Autocomplete\Response;
+
+use PHPUnit_Framework_TestCase;
+
+class ResponseTest extends PHPUnit_Framework_TestCase
+{
+
+    private $instance;
+
+    public function setUp()
+    {
+        //$this->instance = new Autocomplete\Response\Response();
+    }
+
+    public function testEqual()
+    {
+        $this->assertEquals(1, 1);
+    }
+
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,5 @@
+<?php
+
+date_default_timezone_set('UTC');
+
+$autoloader = require_once dirname(__FILE__) . '/../vendor/autoload.php';
